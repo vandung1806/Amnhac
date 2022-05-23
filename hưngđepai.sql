91ce96a461da05a23cbb0867f7cc4f282bcba797
vandung <DungBVTH2108020@fpt.edu.vn>
2022-05-23 17:39:17 +0700

    su mo

---
+++ hưngđepai.sql
@@ -0,0 +1,24 @@
+﻿create database Amnhac
+go
+
+use Amnhac
+go
+
+create table customer(
+	idbaihat int,
+	nameBaihat nvarchar (100),
+	author nvarchar(100),
+	Category nvarchar(100),
+	releaseyear varchar(100),
+	)
+go
+
+insert into customer(idbaihat, nameBaihat, author, Category, releaseyear) values (1, N'Lạ lùng', 'Vũ', 'Love','2017'),
+																				(2, N'Cuấn cho anh một điếu nữa đi', 'MCK','Love', '2022'),
+																				(3,N'Beautiful Girls','Sean Kingston', 'Jazz','2010'),
+																				(4,N'Sweet Emotion', 'Aerosmith ','Rock','1975'),
+																				(5,N'Quan Tài Hư', ' young milo', 'Chơi Đồ','2021')																		
+go																	
+
+select*from customer
+go
